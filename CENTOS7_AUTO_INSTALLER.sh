9bddd755db9522cdf06d141976dc6679a6b76596
zortcoin <86495493+zortcoin@users.noreply.github.com>
2021-07-25 15:46:40 +0000

    Update CENTOS7_AUTO_INSTALLER.sh

--- CENTOS7_AUTO_INSTALLER.sh
+++ CENTOS7_AUTO_INSTALLER.sh
@@ -46,10 +46,10 @@ PASS_WORD="$(env LC_CTYPE=C tr -dc a-zA-Z0-9 < /dev/urandom| head -c 32; echo)"
 mkdir /root/.zortcoin
 
 
-cat <<EOF > "/root/.zortcoin/zortcoin.conf"
-rpcuser=zortcoinrpc
-rpcpassword=${PASS_WORD}
-EOF
+cp /tmp/zortcoin/zortcoin/share/examples/zortcoin.conf /root/.zortcoin/zortcoin.conf
+
+sudo sed -i 's/#rpcuser=alice/rpcuser=zortcoinrpc/g' /root/.zortcoin/zortcoin.conf
+sudo sed -i "s/#rpcpassword=DONT_USE_THIS_YOU_WILL_GET_ROBBED_8ak1gI25KFTvjovL3gAM967mies3E=/rpcpassword=${PASS_WORD}/g" /root/.zortcoin/zortcoin.conf
 
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
@@ -115,4 +115,8 @@ printf "\\n"
 printf "RPC user: zortcoinrpc"
 printf "\\n"
 printf "RPC password: ${PASS_WORD}"
+printf "\\n"
+printf "\\n"
+printf "You can edit this file for many settings: /root/.zortcoin/zortcoin.conf"
+printf "\\n"
 printf "\\n"
